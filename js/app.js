cd13d8164c51556154a58521f30c8089b91bab8d
Carol Gardiner <carolan.gardiner@gmail.com>
2018-04-15 17:57:03 +0100

    Cards not flipping but showing symbols

--- js/app.js
+++ js/app.js
@@ -10,13 +10,13 @@ let card1Parent = '';
 let card2Parent = '';
 let ready = true; 
 let stopTimer = false;
-let cardcounter = 0; 
+let cardCounter = 0; 
 
 document.querySelector(".restart").addEventListener("click", restart);
 document.querySelector(".deck").addEventListener("click", function(){stopTimer = false; timerStart()});
 document.querySelector(".deck").addEventListener("click", cardOpen);
-document.querySelector(".playAgain").addEventListener("click", function() {
-		document.querySelector(".winPage").className = "winPage closed"; restart()}); 
+//document.querySelector(".playAgain").addEventListener("click", function() {
+ // document.querySelector(".winPage").className = "winPage closed"; restart()}); 
 
 
 //  * Display the cards on the page
@@ -48,7 +48,7 @@ if (card1 == false) {
 
       card2 = evt.target.firstElementChild.className; 
       card2Partent =evt.target; 
-      cardcounter = 2; 
+      cardCounter = 2; 
 
       //card matching
 
@@ -56,12 +56,12 @@ if (card1 == false) {
       	card1Parent.className = "card open show match";
       	card2Parent.className = "card open show match";
       	card1 = '';
-      	card 2 = '';
+      	card2 = '';
       	cardCounter = 0; 
       	win ();
-     }else 
-     	setTimeout(function() {
-     		evt.taget.className = "card close"; card1Parent.className = "card close"}, 700);
+     }else {
+     	setTimeout(function () {
+          evt.target.className = "card close"; card1Parent.className = "card close"}, 700);
      	setTimeout(function () {
      		evt.target.className = "card"; card1Parent.className = "card";
      				card1 = ''; card2 = ''; cardCounter = 0}, 900);
@@ -70,9 +70,29 @@ if (card1 == false) {
 ready = false; 
 
 	}
-
 }
 
+
+function restart() {
+  card1 = "";
+  card2 = "";
+	document.querySelector(".moves").innerText = "0";
+	returnStars();
+  document.querySelector(".winPage").className = "winPage closed";
+
+	let cards = Array.prototype.slice.call(document.querySelectorAll('.card'));
+	cards = shuffle(cards);
+	const deck = document.querySelector(".deck");
+
+	for (let i = 0; i < cards.length; i++) {
+		deck.appendChild(cards[i]);
+		cards[i].className = "card";
+	}
+
+	ready = true;
+  stopTimer = true;
+
+ }
 
 // // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -100,3 +120,47 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+// Timer
+
+function timerStart() {
+	if (ready == true) {
+		var timer = 0;
+		var hour = 0;
+		var minute = 0;
+		var second = 0;
+		window.setInterval (function() {
+		  ++timer;
+		  hour = Math.floor(timer / 3600);
+		  minute = Math.floor((timer - hour * 3600) / 60);
+		  second = timer - hour * 3600 - minute * 60;
+		  if (hour < 10) hour = '0' + hour;
+		  if (minute < 10) minute = '0' + minute;
+		  if (second < 10) second = '0' + second;
+		  document.querySelector('#timer').innerHTML = hour + ':' + minute + ':' + second;
+		  if(stopTimer) {
+			document.querySelector('#timer').innerHTML = "00:00:00";
+			timer = 0;
+			hour = 0;
+			minute = 0;
+			second = 0;
+			return;
+		  }
+		}, 1000);
+	}
+}
+
+// Shows a modal box when you win:
+
+function win() {
+ document.querySelector(".movesCount").innerText = document.querySelector(".moves").innerText;
+ document.querySelector(".starsCount").innerText = document.getElementsByClassName("fa-star").length;
+ document.querySelector(".finalTime").innerText = document.querySelector('#timer').innerHTML;
+
+ //Collect cards to check if all are open and match:
+
+ let matchingCards = document.getElementsByClassName('card match open show');
+ if (matchingCards.length == 16) {
+   setTimeout (function() {document.querySelector(".winPage").className = "winPage"}, 1000);
+   stopTimer = true;
+ }
+}
